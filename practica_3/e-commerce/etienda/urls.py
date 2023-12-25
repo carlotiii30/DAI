8d61b4788297d0d7858a58b37a7c3e87e83254ab
Carlota de la Vega <carlotadlavega@correo.ugr.es>
2023-12-25 13:26:28 +0100

    Practica 3

---
+++ practica_3/e-commerce/etienda/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from .api import api
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("busqueda/", views.resultado_busqueda, name="resultado_busqueda"),
+    path("categoria/<str:categoria>", views.categorias, name="categorias"),
+    path("nuevo_producto/", views.crear_producto, name="crear_producto"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("api/", api.urls),
+]
