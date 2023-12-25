918f946ac8e0af43432f228b3722dc90c9b667ab
Carlota de la Vega <carlotadlavega@correo.ugr.es>
2023-12-25 13:49:53 +0100

    Aplicacion de perritos

--- practica_5/src/App.jsx
+++ practica_5/src/App.jsx
@@ -1,9 +1,64 @@
-import Welcome from "./components/Welcome"
+import { useEffect, useState } from "react";
+import "./styles.css";
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [imageUrl, setImageUrl] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (isLoading) {
+      async function fetchData() {
+        try {
+          const response = await fetch(
+            "https://dog.ceo/api/breeds/image/random"
+          );
+          if (response.ok) {
+            const dog = await response.json();
+            setImageUrl(dog.message);
+            setError(null);
+            setIsLoading(false);
+          } else {
+            setError("Hubo un error al obtener el perrito");
+          }
+        } catch (error) {
+          setError("No pudimos hacer la solicitud para obtener el perrito");
+        }
+      }
+      fetchData();
+    }
+  }, [isLoading]);
+
+  const randomDog = () => {
+    setIsLoading(true);
+  };
+
+  if (error) {
+    return (
+      <div className="App">
+        <h1>{error}</h1>
+        <button onClick={randomDog}>Volver a intentarlo</button>
+      </div>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <div className="App">
+        <h1>Cargando...</h1>
+      </div>
+    );
+  }
+
   return (
-    <>
-      <Welcome />
-    </>
-  )
+    <div className="App">
+      <img src={imageUrl} alt="Imagen de perrito aleatoria" />
+      <button onClick={randomDog}>
+        ¡Otro!{" "}
+        <span role="img" aria-label="corazón">
+          ❤️
+        </span>
+      </button>
+    </div>
+  );
 }
